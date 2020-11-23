8f49a290672bd761045108e96112841442808f8c
GANRIQING <48664755+GANRIQING@users.noreply.github.com>
2020-11-23 11:54:26 +0800

    Update 112301.App.java

--- 112301.App.java
+++ 112301.App.java
@@ -4,13 +4,13 @@ public class App {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        //输入坐标数
         int n = sc.nextInt();
-        while (n>0){
-            int x = (int)(Math.random()*100);
-            int y = (int)(Math.random()*100);
-            System.out.println("("+x+","+y+")");
-            n--;
+        while (true){
+           if(n==1){
+	            int x = (int)(Math.random()*100);
+	            int y = (int)(Math.random()*100);
+                System.out.println("("+x+","+y+")");
+           }
         }
     }
 }
