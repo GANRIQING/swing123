186807a4d45d3b8162cd3cd44627094064a6a03f
GANRIQING <48664755+GANRIQING@users.noreply.github.com>
2020-11-30 11:59:55 +0800

    Create 113002.R.java

---
+++ 113002.R.java
@@ -0,0 +1,6 @@
+interface R {
+    String boy_filePath="img/boy.gif";
+    String girl_filePath="img/girl.gif";
+    int [][]arr={{1,2,3},{4,5,6}};
+    void fun(String boy_filePath,String girl_filePath, int[][] arr);
+}
