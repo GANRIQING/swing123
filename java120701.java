3c4fefeb258f9e649dcaa5cb8120b238a618d578
GANRIQING <48664755+GANRIQING@users.noreply.github.com>
2020-12-07 12:08:00 +0800

    Create java120701.java

---
+++ java120701.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        int[][] arr = {{1,2,3},{0,5,6},{7,8,9}};
+        Find find = new Find();
+        find.findZero(arr);
+    }
+}
